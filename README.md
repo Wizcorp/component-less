e1e4c95bed45dcbcceacd4df4febdf72bd312810
Micky Brunetti <mbrunetti@wizcorp.jp>
2013-07-08 10:15:58 +0900

    * First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# component-less
+
+A plugin to transpile Less files for the component builder.
+
+## Install
+
+$ npm install component-less
+
+## Usage
+  
+Add your `.less` files to the `styles` array in your `component.json`:
+
+```javascript
+{
+	"styles": [
+		"base.less",
+		"button.css"
+	]
+}
+```
+
+Use the plugin during your build process:
+
+```javascript
+var Builder = require('component-builder');
+var less = require('component-less');
+var fs = require('fs');
+
+var builder = new Builder(__dirname);
+
+builder.use(less);
+
+builder.build(function (err, res) {
+	if (err) {
+		throw err
+	}
+
+	fs.writeFileSync('build/build.js', res.require + res.js);
+
+	if (res.css) {
+		fs.writeFileSync('build/build.css', res.css);
+	}
+});
+```
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,61 @@
+var async = require('async');
+var path = require('path');
+var less = require('less');
+var fs = require('fs');
+
+
+function isLess(filename) {
+	var ext = path.extname(filename);
+	if (ext === '.less') {
+		return true;
+	}
+	return false;
+}
+
+
+module.exports = function (builder, options) {
+
+	options = options || {};
+
+	builder.hook('before styles', function (builder, callback) {
+		if (!builder.config.styles) {
+			return callback();
+		}
+
+		var files = builder.config.styles;
+		var parser = new less.Parser();
+
+		async.forEach(files, function (file, cb) {
+
+			var stylesheet = builder.path(file);
+
+			if (!fs.existsSync(stylesheet)) {
+				return cb(new Error("'" + stylesheet + "'  doesn't exist."));
+			}
+
+			if (!options.compress && !isLess(file)) {
+				return cb();
+			}
+
+			var data = fs.readFileSync(stylesheet, 'utf8');
+
+			parser.parse(data, function (error, tree) {
+				if (error) {
+					return cb(error);
+				}
+
+				var css = tree.toCSS(options);
+
+
+				var newFile = path.basename(file, path.extname(file)) + ((options.minify) ? '-compressed' : '') + '.css';
+				builder.addFile('styles', newFile, css);
+				builder.removeFile('styles', file);
+
+				cb();
+			});
+
+
+		}, callback);
+
+	});
+};
